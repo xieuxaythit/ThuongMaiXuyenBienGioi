847592b4270b002fcb7c7aaf10062206650e8f0c
Xieu  Xay Thit <mr.khas@gmail.com>
2022-04-25 09:57:42 +0700

    Push

--- contracts/HopDongMuaBan.sol
+++ contracts/HopDongMuaBan.sol
@@ -3,14 +3,22 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract HopDongThuongMai {
-    address payable public TaiKhoanNguoiMua;
-    address payable public TaiKhoanNguoiBan;
-    address payable public TaiKhoanVanChuyen;
 
-    uint    public TienHang;        // Tinh bang gwei (mot phan ty cua ETH)
-    uint    public TienVanChuyen;   // Tinh bang gwei (mot phan ty cua ETH)
-    uint256 public ToaDoGiaoHang;   // Ma bam cua toa do GPS
-    uint    public ThoiHanHopDong;
+    struct HopDong {
+        address payable TaiKhoanNguoiMua;
+        address payable TaiKhoanNguoiBan;
+        address payable TaiKhoanVanChuyen;
+
+        uint    TienHang;        // Tinh bang gwei (mot phan ty cua ETH)
+        uint    TienVanChuyen;   // Tinh bang gwei (mot phan ty cua ETH)
+        uint256 ToaDoGiaoHang;   // Ma bam cua toa do GPS
+        uint    ThoiHanHopDong;
+
+        enTrangThaiHopDong TrangThaiHopDong;
+    }
+
+    uint SoHopDong;
+    mapping (uint => HopDong) DanhSachHopDong;    
 
     enum enTrangThaiHopDong {
         Moi,
@@ -22,160 +30,208 @@ contract HopDongThuongMai {
         ThatBai
     }
 
-    enTrangThaiHopDong public TrangThaiHopDong;
-
     event NguoiMuaDaDatCocRoi(uint256 intToaDoGiaoHang);
     event HangDangTrenDuongRoi();
     event HopDongThanhCongRoi();
+    event HopDongDangChoPhanXu();
 
-    constructor (address payable adNguoiMua, address payable adNguoiBan, uint intTienHang, uint intTienVanChuyen) {
-        TaiKhoanNguoiBan    = adNguoiBan;
-        TaiKhoanNguoiMua    = adNguoiMua;
-        TienHang            = intTienHang;      // tinh bang gwei
-        TienVanChuyen       = intTienVanChuyen; // tinh bang gwei
-        //ToaDoGiaoHang       = intToaDoGiaoHang;
-        ThoiHanHopDong      = block.timestamp + 30 days;
+    // constructor (address payable adNguoiMua, address payable adNguoiBan, uint intTienHang, uint intTienVanChuyen) {
+    //     TaiKhoanNguoiBan    = adNguoiBan;
+    //     TaiKhoanNguoiMua    = adNguoiMua;
+    //     TienHang            = intTienHang;      // tinh bang gwei
+    //     TienVanChuyen       = intTienVanChuyen; // tinh bang gwei
+    //     //ToaDoGiaoHang       = intToaDoGiaoHang;
+    //     ThoiHanHopDong      = block.timestamp + 30 days;
         
-        TrangThaiHopDong    = enTrangThaiHopDong.Moi;
+    //     TrangThaiHopDong    = enTrangThaiHopDong.Moi;
+    // }
+
+    function TaoHopDongMoi(address payable adNguoiMua, address payable adNguoiBan, uint intTienHang, uint intTienVanChuyen) public returns (uint intMaSoHopDong) {
+        //Tao ma so hop dong
+        intMaSoHopDong = SoHopDong++;
+
+        // Khoi tao hop dong moi trong danh sach voi ma so vua tao
+        DanhSachHopDong[intMaSoHopDong] = HopDong({
+                                                    TaiKhoanNguoiMua:   adNguoiMua, 
+                                                    TaiKhoanNguoiBan:   adNguoiBan, 
+                                                    TaiKhoanVanChuyen:  payable(address(0)),
+                                                    TienHang:           intTienHang, 
+                                                    TienVanChuyen:      intTienVanChuyen, 
+                                                    ToaDoGiaoHang:      0, 
+                                                    ThoiHanHopDong:     block.timestamp + 30 days,
+                                                    TrangThaiHopDong:   enTrangThaiHopDong.Moi
+                                                });
+
+        // HopDong storage stHopDong   = DanhSachHopDong[MaSoHopDong];
+        // stHopDong.TaiKhoanNguoiMua  = adNguoiMua;
+        // stHopDong.TaiKhoanNguoiBan  = adNguoiBan;
+        // stHopDong.TienHang          = intTienHang;
+        // stHopDong.TienVanChuyen     = intTienVanChuyen;
+
+        return intMaSoHopDong;
     }
 
-    function DatCocMuaHang(uint256 intToaDoGiaoHang) payable public {
+    function DatCocMuaHang(uint intMaSoHopDong, uint256 intToaDoGiaoHang) payable public {
         
+        // Lay hop dong tu danh sach theo intMaSoHopDong
+        HopDong storage stHopDong = DanhSachHopDong[intMaSoHopDong];
+
         // Kiem tra dieu kien trang thai hop dong va so tien
-        require(TrangThaiHopDong == enTrangThaiHopDong.Moi);
-        require(msg.value >= ((TienHang + TienVanChuyen) * (1 gwei)));
+        require(stHopDong.TrangThaiHopDong == enTrangThaiHopDong.Moi);
+        require(msg.value >= ((stHopDong.TienHang + stHopDong.TienVanChuyen) * (1 gwei)));
 
         // Cap nhat trang thai va du lieu hop dong
-        TrangThaiHopDong = enTrangThaiHopDong.NguoiMuaDaDatCoc;
-        ToaDoGiaoHang = intToaDoGiaoHang;
+        stHopDong.TrangThaiHopDong = enTrangThaiHopDong.NguoiMuaDaDatCoc;
+        stHopDong.ToaDoGiaoHang = intToaDoGiaoHang;
 
-        // Thong bao cho nguoi bang
+        // Thong bao cho nguoi ban hang
         emit NguoiMuaDaDatCocRoi(intToaDoGiaoHang);
     }
 
-    function DatCocVanChuyen() payable public {
+    function DatCocVanChuyen(uint intMaSoHopDong) payable public {
+
+        // Lay hop dong tu danh sach theo intMaSoHopDong
+        HopDong storage stHopDong = DanhSachHopDong[intMaSoHopDong];
 
         // Kiem tra xem hop dong da dat coc chua
-        require(TrangThaiHopDong == enTrangThaiHopDong.NguoiMuaDaDatCoc);
+        require(stHopDong.TrangThaiHopDong == enTrangThaiHopDong.NguoiMuaDaDatCoc);
 
         // Kiem tra tien coc van chuyen tuong duong tien hang
-        require(msg.value == TienHang * (1 gwei) );
+        require(msg.value == stHopDong.TienHang * (1 gwei) );
         
         // Cap nhat trang thai hop dong
-        TrangThaiHopDong = enTrangThaiHopDong.HangDangGiao;
+        stHopDong.TrangThaiHopDong = enTrangThaiHopDong.HangDangGiao;
 
         // Cap nhat nguoi van chuyen tien
-        TaiKhoanVanChuyen = payable(msg.sender);
+        stHopDong.TaiKhoanVanChuyen = payable(msg.sender);
 
         // Thong bao cho cac ben la hang dang tren duong roi
         emit HangDangTrenDuongRoi();
     }
 
-    function HangDaToiDich(uint256 intToaDoGiaoToi) public {
+    function HangDaToiDich(uint intMaSoHopDong, uint256 intToaDoGiaoToi) public {
+
+        // Lay hop dong tu danh sach theo intMaSoHopDong
+        HopDong storage stHopDong = DanhSachHopDong[intMaSoHopDong];
 
         // Kiem tra xem co dung nguoi van chuyen khong, CHI CHO PHEP NGUOI VAN CHUYEN GOI HAM NAY
-        require (msg.sender == TaiKhoanVanChuyen);
+        require (msg.sender == stHopDong.TaiKhoanVanChuyen);
 
         // Kiem tra thoi gian giao hang trong han hop dong
-        require (block.timestamp <= ThoiHanHopDong);
+        require (block.timestamp <= stHopDong.ThoiHanHopDong);
 
         //Kiem tra trang thai hop dong
-        require(TrangThaiHopDong == enTrangThaiHopDong.HangDangGiao);
+        require(stHopDong.TrangThaiHopDong == enTrangThaiHopDong.HangDangGiao);
 
         // So khop toa do giao hang
-        require(intToaDoGiaoToi == ToaDoGiaoHang);
+        require(intToaDoGiaoToi == stHopDong.ToaDoGiaoHang);
 
         // Cap nhat trang thai hop dong khi giao hang dung toa do
-        TrangThaiHopDong = enTrangThaiHopDong.HangDaToiDichRoi;
+        stHopDong.TrangThaiHopDong = enTrangThaiHopDong.HangDaToiDichRoi;
     }
 
-    function NguoiMuaXacNhanHang(bool bHangOK) payable public {
+    function NguoiMuaXacNhanHang(uint intMaSoHopDong, bool bHangOK) payable public {
+
+        // Lay hop dong tu danh sach theo intMaSoHopDong
+        HopDong storage stHopDong = DanhSachHopDong[intMaSoHopDong];
 
         // Kiem tra xem co dung nguoi mua khong, CHI CHO PHEP NGUOI MUA GOI HAM NAY
-        require (msg.sender == TaiKhoanNguoiMua);
+        require (msg.sender == stHopDong.TaiKhoanNguoiMua);
 
         // Thanh ly hop dong neu hang OK, neu hang khong OK thi cho xac nhan tiep theo cua nguoi van chuyen
         if(bHangOK == false) {
-            TrangThaiHopDong = enTrangThaiHopDong.ChoPhanXu;
+            // Cap nhat trang thai hop dong dang bi tranh chap, cho phan xu.
+            stHopDong.TrangThaiHopDong = enTrangThaiHopDong.ChoPhanXu;
+
+            // Thong bao cho cac ben ve trang thai hop dong dang cho phan xu
+            emit HopDongDangChoPhanXu();
         }
         else {
             // Cap nhat trang thai hop duong
-            TrangThaiHopDong = enTrangThaiHopDong.ThanhCong;
+            stHopDong.TrangThaiHopDong = enTrangThaiHopDong.ThanhCong;
 
             // Tra tien cho nguoi ban
-            TaiKhoanNguoiBan.transfer( TienHang * (1 gwei) );
+            stHopDong.TaiKhoanNguoiBan.transfer( stHopDong.TienHang * (1 gwei) );
 
             // Tra tien cho nguoi van chuyen (coc van chuyen + phi van chuyen)
-            TaiKhoanVanChuyen.transfer( (TienHang + TienVanChuyen) * (1 gwei) );
+            stHopDong.TaiKhoanVanChuyen.transfer( (stHopDong.TienHang + stHopDong.TienVanChuyen) * (1 gwei) );
 
             // Vet het tien con lai trong CONTRACT tra lai cho nguoi mua
-            TaiKhoanNguoiMua.transfer( address(this).balance );
+            stHopDong.TaiKhoanNguoiMua.transfer( address(this).balance );
 
+            // Thong bao cho cac ben la hop dong da thanh cong.
             emit HopDongThanhCongRoi();
         }
     }
 
-    function NguoiVanChuyenXacNhanHang(bool bHangOK) payable public {
+    function NguoiVanChuyenXacNhanHang(uint intMaSoHopDong, bool bHangOK) payable public {
         
-        // Kiem tra xem co dung nguoi van chuyen khong, CHI CHO PHEP NGUOI VAN CHUYEN GOI HAM NAY
-        require (msg.sender == TaiKhoanVanChuyen);
+        // Lay hop dong tu danh sach theo intMaSoHopDong
+        HopDong storage stHopDong = DanhSachHopDong[intMaSoHopDong];
 
-        require (TrangThaiHopDong == enTrangThaiHopDong.ChoPhanXu);
+        // Kiem tra xem co dung nguoi van chuyen khong, CHI CHO PHEP NGUOI VAN CHUYEN GOI HAM NAY
+        require (msg.sender == stHopDong.TaiKhoanVanChuyen);
+
+        require (stHopDong.TrangThaiHopDong == enTrangThaiHopDong.ChoPhanXu);
 
         if(bHangOK == false) {
-            // Nguoi van chuyen cong nhanh voi Nguoi mua hang bi hong 
+            // Nguoi van chuyen cong nhan voi Nguoi mua hang bi hong 
             // Van tra tien cho nguoi ban binh thuong
-            TaiKhoanNguoiBan.transfer( TienHang * (1 gwei) );
+            stHopDong.TaiKhoanNguoiBan.transfer( stHopDong.TienHang * (1 gwei) );
 
             // Tra lai toan bo tien con lai trong CONTRACT cho nguoi mua 
-            TaiKhoanNguoiMua.transfer( address(this).balance );
+            stHopDong.TaiKhoanNguoiMua.transfer( address(this).balance );
         }
         else {
             // Nguoi van chuyen khong cong nhan hang hong, cho rang HangOK = TRUE
+            // can xu ly them truong hop nay.
 
         }
     }
 
-    function ThanhLyHopDong() payable public {
+    function ThanhLyHopDong(uint intMaSoHopDong) payable public {
         
+        // Lay hop dong tu danh sach theo intMaSoHopDong
+        HopDong storage stHopDong = DanhSachHopDong[intMaSoHopDong];
+
         // Kiem tra truong hop qua han moi duoc thanh ly
-        require (block.timestamp > ThoiHanHopDong);
+        require (block.timestamp > stHopDong.ThoiHanHopDong);
 
         // Chi cho cac ben tham gia thanh ly hop dong
-        require( msg.sender == TaiKhoanNguoiMua || msg.sender == TaiKhoanNguoiBan || msg.sender == TaiKhoanVanChuyen );
+        require( msg.sender == stHopDong.TaiKhoanNguoiMua || msg.sender == stHopDong.TaiKhoanNguoiBan || msg.sender == stHopDong.TaiKhoanVanChuyen );
 
         // Thanh ly hop dong chi moi dat coc ma chua lam gi
-        if (TrangThaiHopDong == enTrangThaiHopDong.NguoiMuaDaDatCoc) {
+        if (stHopDong.TrangThaiHopDong == enTrangThaiHopDong.NguoiMuaDaDatCoc) {
             // Tra lai tien cho nguoi mua toan bo so tien trong CONTRACT 
-            TaiKhoanNguoiMua.transfer( address(this).balance );
+            stHopDong.TaiKhoanNguoiMua.transfer( address(this).balance );
         } 
 
         // Thanh ly hop dong dang giao hang thi chim tau 
-        if (TrangThaiHopDong == enTrangThaiHopDong.HangDangGiao) {
+        if (stHopDong.TrangThaiHopDong == enTrangThaiHopDong.HangDangGiao) {
             // Tra lai tien cho nguoi mua va nguoi ban
-            TaiKhoanNguoiBan.transfer( TienHang * (1 gwei) );
-            TaiKhoanNguoiMua.transfer( address(this).balance );
+            stHopDong.TaiKhoanNguoiBan.transfer( stHopDong.TienHang * (1 gwei) );
+            stHopDong.TaiKhoanNguoiMua.transfer( address(this).balance );
         }
 
         // Thanh ly hop dong khi den noi thi nguoi mua "CHET" hoac khong nhan hang
-        if (TrangThaiHopDong == enTrangThaiHopDong.HangDaToiDichRoi) {
+        if (stHopDong.TrangThaiHopDong == enTrangThaiHopDong.HangDaToiDichRoi) {
             // Chia ra 2 truong hop, thanh ly ngay, hoac la mang hang ve tra lai NGUOI BAN
             // TRUONG HOP THANH LY NGAY vi den day nguoi ban va nguoi van chuyen da hoan thanh trach nhiem
             // Tra tien cho nguoi ban binh thuong
-            TaiKhoanNguoiBan.transfer( TienHang * (1 gwei) );
+            stHopDong.TaiKhoanNguoiBan.transfer( stHopDong.TienHang * (1 gwei) );
 
             // Tra tien cho nguoi van chuyen binh thuong 
-            TaiKhoanVanChuyen.transfer( (TienHang + TienVanChuyen) * (1 gwei) );
+            stHopDong.TaiKhoanVanChuyen.transfer( (stHopDong.TienHang + stHopDong.TienVanChuyen) * (1 gwei) );
 
             // Vet not tien con du trong CONTRACT tra lai cho nguoi mua 
-            TaiKhoanNguoiMua.transfer( address(this).balance );
+            stHopDong.TaiKhoanNguoiMua.transfer( address(this).balance );
         }
 
         // Thanh ly hop dong dang cho phan xu
-        if (TrangThaiHopDong == enTrangThaiHopDong.ChoPhanXu) {
+        if (stHopDong.TrangThaiHopDong == enTrangThaiHopDong.ChoPhanXu) {
             // Tra lai tien cho nguoi mua va nguoi ban
-            TaiKhoanNguoiBan.transfer( TienHang * (1 gwei) );
-            TaiKhoanNguoiMua.transfer( address(this).balance );
+            stHopDong.TaiKhoanNguoiBan.transfer( stHopDong.TienHang * (1 gwei) );
+            stHopDong.TaiKhoanNguoiMua.transfer( address(this).balance );
         }
 
     }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 var HopDongThuongMai = artifacts.require("HopDongThuongMai");
 
-var adNguoiMua          = "0x7edbF50d5a71408914a280768C1aE724dF43dAa6", 
-    adNguoiBan          = "0x0d8E1dd1C9BC92559F8317dfc50F2ADD080D3f24", 
-    intTienHang         = 100000000000, 
-    intTienVanChuyen    = 10000000000;
+// var adNguoiMua          = "0x7edbF50d5a71408914a280768C1aE724dF43dAa6", 
+//     adNguoiBan          = "0x0d8E1dd1C9BC92559F8317dfc50F2ADD080D3f24", 
+//     intTienHang         = 100000000000, 
+//     intTienVanChuyen    = 10000000000;
 
 module.exports = deployer => {
-    deployer.deploy(HopDongThuongMai, adNguoiMua, adNguoiBan, intTienHang, intTienVanChuyen);
+    deployer.deploy(HopDongThuongMai);
 };
